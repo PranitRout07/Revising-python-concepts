50db32d2015a5df3c945b88bf0680552018b37d2
PranitRout07 <pranitrout72@gmail.com>
2024-02-16 19:37:21 +0530

    creating a new job from empty configuration

--- Exploring Jenkins API/main.py
+++ Exploring Jenkins API/main.py
@@ -11,3 +11,11 @@ server = jenkins.Jenkins(host,username=username,password=password)
 user = server.get_whoami()
 print("Hello",user['fullName'])
 
+#creating a job 
+jobs = server.get_jobs()
+if 'empty' in jobs:
+    server.create_job('empty', jenkins.EMPTY_CONFIG_XML)
+
+for i in jobs:
+    if i['name']=='empty':
+        print(i)
